ab44fcb28cba78504e010811b25778929bd66e38
Ikbar mahesa <ikbar.mahesa9@gmail.com>
2016-03-29 15:57:02 +0700

    modified sum of fibonacci

--- Jurnalfibo.java
+++ Jurnalfibo.java
@@ -4,14 +4,16 @@ package jurnal;
 import java.util.Scanner;
 
 public class Jurnalfibo {
+
         public static void main(String[] args){
 		Scanner a = new Scanner(System.in);
 		System.out.print("Inputan: ");
 		int n= a.nextInt();
+                int hasil=0;
 		for(int i=1;i<=n;i++){
-                    System.out.print(jun(i) + " ");
+                    hasil =hasil +jun(i);
                 }	
-		System.out.println();
+		System.out.println(hasil);
         }
 	private static int jun(int n){
 		int f;
